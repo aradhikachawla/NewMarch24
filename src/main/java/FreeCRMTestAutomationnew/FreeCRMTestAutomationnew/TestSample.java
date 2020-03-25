25cebf9a137a9f2efb6c281501a9e58a587a9b46
Aradhika Chawla <aradhikac@gmail.com>
2020-03-24 19:41:09 -0700

    ffff

---
+++ src/main/java/FreeCRMTestAutomationnew/FreeCRMTestAutomationnew/TestSample.java
@@ -0,0 +1,35 @@
+package FreeCRMTestAutomationnew.FreeCRMTestAutomationnew;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class TestSample  {
+   
+	@Test
+	public  void test1() {
+
+        // Set the property for webdriver.chrome.driver to be the location to your local              download of chromedriver
+    //    System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+    	System.getProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/chromedriver 2");
+
+        // Create new instance of ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // And now use this to visit Google
+        driver.get("http://www.google.com");
+
+        // Find the text input element by its name
+        WebElement element = driver.findElement(By.name("q"));
+
+        // Enter something to search for
+        element.sendKeys("Cheese!");
+
+        // Now submit the form
+        element.submit();
+
+        //Close the browser
+       // driver.quit();
+    }
+}
